f00b0b19546c5d42e50d81074e5d93705d78120e
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-03 01:19:13 +0900

    Update TVHotPlugin.kt

--- TVHotProvider/src/main/kotlin/com/tvhot/TVHotPlugin.kt
+++ TVHotProvider/src/main/kotlin/com/tvhot/TVHotPlugin.kt
@@ -7,7 +7,9 @@ import android.content.Context
 @CloudstreamPlugin
 class TVHotPlugin: Plugin() {
     override fun load(context: Context) {
+        // TVHot 메인 클래스 등록
         registerMainAPI(TVHot())
+        // BunnyPoorCdn 추출기 등록
         registerExtractorAPI(BunnyPoorCdn())
     }
 }
