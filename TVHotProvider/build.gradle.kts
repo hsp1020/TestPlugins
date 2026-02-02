2d02b0d04665f082780a5f18de5de2c216dcc9fb
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-03 00:42:55 +0900

    Update build.gradle.kts

--- TVHotProvider/build.gradle.kts
+++ TVHotProvider/build.gradle.kts
@@ -2,7 +2,7 @@ version = 1
 
 cloudstream {
     description = "한국 드라마 및 TV 프로그램 다시보기 제공"
-    authors = listOf("hsp10202") // 본인 계정명으로 변경
+    authors = listOf("hsp1020") // 본인 계정명으로 변경
     language = "ko"
     
     status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
