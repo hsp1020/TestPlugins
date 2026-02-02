ba8bafc068a8be80e511e1e0e14b843a608f6ea6
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-03 00:40:02 +0900

    Create build.gradle.kts

---
+++ TVHotProvider/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 1
+
+cloudstream {
+    description = "한국 드라마 및 TV 프로그램 다시보기 제공"
+    authors = listOf("hsp10202") // 본인 계정명으로 변경
+    language = "ko"
+    
+    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime"
+    )
+    
+    iconUrl = "https://tvhot.store/img/logo.png"
+}
