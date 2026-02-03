bd05f5fcd8d6542c21c0da4870eec5bbed4f1d3c
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-03 15:45:46 +0900

    Update TVHotPlugin.kt

--- TVHotProvider/src/main/kotlin/com/tvhot/TVHotPlugin.kt
+++ TVHotProvider/src/main/kotlin/com/tvhot/TVHotPlugin.kt
@@ -1,16 +1,12 @@
 package com.tvhot
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class TVHotPlugin: Plugin() {
-    override fun load(context: Context) {
-        // TVHot 메인 프로바이더 등록
+class TVHotPlugin: BasePlugin() {
+    override fun load() {
         registerMainAPI(TVHot())
-        
-        // Extractor 등록 (BunnyPoorCdn)
         registerExtractorAPI(BunnyPoorCdn())
     }
 }
