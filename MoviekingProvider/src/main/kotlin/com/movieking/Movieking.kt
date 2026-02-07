9982113175e09713fa6c488d5014273dc3ff7bf7
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-08 07:48:41 +0900

    Update Movieking.kt

--- MoviekingProvider/src/main/kotlin/com/movieking/Movieking.kt
+++ MoviekingProvider/src/main/kotlin/com/movieking/Movieking.kt
@@ -21,7 +21,6 @@ class MovieKing : MainAPI() {
     )
 
     private val commonHeaders = mapOf(
-        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "Referer" to "$mainUrl/"
     )
 
