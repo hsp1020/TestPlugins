12b2db8356bd3985feb7eb64cedea4296430b275
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-12 20:37:08 +0900

    Update Extractor.kt

--- TVWikiProvider/src/main/kotlin/com/tvwiki/Extractor.kt
+++ TVWikiProvider/src/main/kotlin/com/tvwiki/Extractor.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.network.WebViewResolver 
 import android.webkit.CookieManager
