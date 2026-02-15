3fffb85de877dfbb9e8e82c069237f73e47be00f
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-15 10:23:52 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "CloudstreamPlugins"
 
 // 모든 자동 감지 로직을 지우고 딱 님의 플러그인 폴더만 지정합니다.
-include("TVMonProvider")
+include("TVMONProvider")
 include("TVWikiProvider")
 include("MoviekingProvider")
