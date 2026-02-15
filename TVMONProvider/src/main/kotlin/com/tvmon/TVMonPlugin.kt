d80cbdb3ba275af8f6c2caad8462254d94f61efc
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-02-15 10:37:12 +0900

    Update TVMonPlugin.kt

--- TVMONProvider/src/main/kotlin/com/tvmon/TVMonPlugin.kt
+++ TVMONProvider/src/main/kotlin/com/tvmon/TVMonPlugin.kt
@@ -1,5 +1,6 @@
 package com.tvmom
-
+import com.tvmon.TVMon // 예시 경로
+import com.tvmon.BunnyPoorCdn
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
